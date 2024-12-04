0e9b7e68f0fd3c042c9ec9723f53881e31d191ff
Dalvzam <diego.alvzam@educa.jcyl.es>
2024-12-04 19:42:23 +0100

    prueba

--- app/src/main/java/com/example/androidhelloworld/MainActivity.java
+++ app/src/main/java/com/example/androidhelloworld/MainActivity.java
@@ -16,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
